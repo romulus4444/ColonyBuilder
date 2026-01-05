bcd6a360ee49d7cadae41c66e2b9fbed6d9ce91c
romulus4444 <welch.raymond@gmail.com>
2026-01-04 21:48:40 -0600

    Basic animated character sprites
    Added some free animated sprites
    Set up a character to move around
    Adjusted animation speeds to look ok

---
+++ Assets/Images/Sprites/Free Bundle/ReadMe.txt
@@ -0,0 +1,41 @@
+This free version includes: 
+
+-All the base bodies spritesheets (child, man, woman, chubby man, chubby woman.) 
+with all the skin colors (20 colors.)
+
+-Basic animations (idle, walking, running.)
+
+-3 angles (up, down, left/right.)
+
+-One set of eyes with the basic eye color, and blinking eyes (closed eyes).
+
+
+Included with the face is a png for reference of where 
+the eyes should be on the character's face.
+
+
+------------------------------
+Paid version of Top-Down characters bundle includes:
+
+-29 faces (Eyes, along with 22 eye colors for all eyes except 3.)
+
+-60+ hairstyles with different colors and a grey scale to use any color or have the player choose the color. (Use 50% of any color on top of the grey.)
+
+-3 Faces expressing emotions. (Hurt, surprised and shocked.) 
+
+-20+ Facial hair styles.​
+
+*More will be added in the future*
+
+
+Any character animations released with these bundles will be available 
+as a free update for the paid version of Top-Down Characters bundle.
+
+
+LICENSE:
+
+YES -> Using the assets however you like. No credit needed, but would be appreciated.
+NO -> Reselling the assets.
+
+*The price will increase as more assets are added.
+
---
+++ character.gd
@@ -0,0 +1,81 @@
+extends Area2D
+
+@export var speed = 50
+var direction = "down"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		direction = "right"
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+		direction = "left"
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+		direction = "down"
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		direction = "up"
+	
+	if velocity.length() == 0:
+		if direction == "down":
+			$AnimatedSprite2D.animation = "idle_front"
+			$AnimatedSprite2D.flip_h = false
+		elif direction == "up":
+			$AnimatedSprite2D.animation = "idle_back"
+			$AnimatedSprite2D.flip_h = false
+		elif direction == "right":
+			$AnimatedSprite2D.animation = "idle_right"
+			$AnimatedSprite2D.flip_h = false
+		elif direction == "left":
+			$AnimatedSprite2D.animation = "idle_right"
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.animation = "idle_front"
+			$AnimatedSprite2D.flip_h = false
+	elif velocity.length() > 0:
+		if Input.is_action_pressed("run"):
+			velocity = velocity.normalized() * speed * 3
+			if direction == "down":
+				$AnimatedSprite2D.animation = "run_front"
+				$AnimatedSprite2D.flip_h = false
+			elif direction == "up":
+				$AnimatedSprite2D.animation = "run_back"
+				$AnimatedSprite2D.flip_h = false
+			elif direction == "right":
+				$AnimatedSprite2D.animation = "run_right"
+				$AnimatedSprite2D.flip_h = false
+			elif direction == "left":
+				$AnimatedSprite2D.animation = "run_right"
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.animation = "run_front"
+				$AnimatedSprite2D.flip_h = false
+		else:
+			velocity = velocity.normalized() * speed
+			if direction == "down":
+				$AnimatedSprite2D.animation = "walk_front"
+				$AnimatedSprite2D.flip_h = false
+			elif direction == "up":
+				$AnimatedSprite2D.animation = "walk_back"
+				$AnimatedSprite2D.flip_h = false
+			elif direction == "right":
+				$AnimatedSprite2D.animation = "walk_right"
+				$AnimatedSprite2D.flip_h = false
+			elif direction == "left":
+				$AnimatedSprite2D.animation = "walk_right"
+				$AnimatedSprite2D.flip_h = true
+			else:
+				$AnimatedSprite2D.animation = "walk_front"
+				$AnimatedSprite2D.flip_h = false
+	
+	$AnimatedSprite2D.play()
+	position += velocity * delta
